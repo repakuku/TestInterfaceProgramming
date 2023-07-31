18be31ad1e8cf3fff1f9d637521cdcb3531d93c9
Alexey Turulin <repakuku@icloud.com>
2023-07-31 15:15:25 -0400

    Initial Commit

---
+++ TestInterfaceProgramming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestInterfaceProgramming
+//
+//  Created by Алексей Турулин on 7/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
